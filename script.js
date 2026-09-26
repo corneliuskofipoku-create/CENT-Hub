1a19b0fc4bfafcf0b20ab7b0f09212a7e92b923a
corneliuskofipoku-create <corneliuskofipoku@gmail.com>
2026-09-25 18:25:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const products = [
   { name: "Minimal Smile Tee", category: "T-Shirts", color: "White", price: 180, image: "images/t-shirts/minimal-smile-tee.jpg" },
   { name: "Graphic Street Tee", category: "T-Shirts", color: "Black", price: 200, image: "images/t-shirts/graphic-street-tee.jpg" },
   { name: "Cross Graphic Tee", category: "T-Shirts", color: "White", price: 200, image: "images/t-shirts/cross-graphic-tee.jpg" },
-  { name: "Comic Boom Hoodie", category: "Hoodies", color: "White / Black / Orange", image: "images/hoodies/comic-boom-hoodie.jpg" },
+  { name: "Comic Boom Hoodie", category: "Hoodies", color: "White / Black / Orange", price: 350, image: "images/hoodies/comic-boom-hoodie.jpg" },
   { name: "Classic Black Belt", category: "Accessories", color: "Black", image: "images/accessories/classic-black-belt.jpg" },
   { name: "Flower Link Bracelet", category: "Accessories", color: "Gold / White", image: "images/accessories/flower-link-bracelet.jpg" },
   { name: "Gold Chronograph Watch", category: "Accessories", color: "Gold / Black", image: "images/accessories/gold-chronograph-watch.jpg" }
