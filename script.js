6b5495c0dc19d48f971600cce7fd909c18b68265
corneliuskofipoku-create <corneliuskofipoku@gmail.com>
2026-09-25 18:17:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const whatsappNumber = "233240440655";
 const products = [
   { name: "Cross Detail Cap", category: "Caps & Hats", color: "Black", price: 150, image: "images/caps-hats/cross-detail-cap.jpg" },
   { name: "Classic Bulls Cap", category: "Caps & Hats", color: "Black / Red", price: 180, image: "images/caps-hats/classic-bulls-cap.jpg" },
-  { name: "Color Stack Cap", category: "Caps & Hats", color: "Blue / Red / Green", image: "images/caps-hats/color-stack-cap.jpg" },
+  { name: "Color Stack Cap", category: "Caps & Hats", color: "Blue / Red / Green", price: 170, image: "images/caps-hats/color-stack-cap.jpg" },
   { name: "Essential Trucker Cap", category: "Caps & Hats", color: "Black", image: "images/caps-hats/essential-trucker-cap.jpg" },
   { name: "KING Beanie", category: "Caps & Hats", color: "Black / White / Grey", image: "images/caps-hats/king-beanie.jpg" },
   { name: "Retro Runner Sneakers", category: "Shoes", color: "White / Black", image: "images/shoes/retro-runner-sneakers.jpg" },
